3ae38deb8462ccd8d853738fb8b0419eb16fab10
GLLSLP <jimotme030@naver.com>
2022-04-25 17:39:16 +0900

    [#220425] [6일차]SpringBoot 기반으로 서버 개발하기-6

--- Spring-workspace/example-spring/src/main/java/com/example/demo/configuration/WebConfiguration.java
+++ Spring-workspace/example-spring/src/main/java/com/example/demo/configuration/WebConfiguration.java
@@ -1,15 +1,41 @@
 package com.example.demo.configuration;
 
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.MediaType;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+import kr.co.songjava.configuration.servlet.handler.BaseHandlerInterceptor;
+import kr.co.songjava.framework.data.web.MySQLPageRequestHandleMethodArgumentResolver;
+import kr.co.songjava.mvc.domain.BaseCodeLabelEnum;
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.MediaType;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import com.example.demo.configuration.servlet.handler.BaseHandlerInterceptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
+import kr.co.songjava.configuration.BaseCodeLabelEnumJsonSerializer;
+import kr.co.songjava.framework.data.web.MySQLPageRequestHandleMethodArgumentResolver;
+import kr.co.songjava.mvc.domain.BaseCodeLabelEnum;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -30,9 +56,32 @@ public class WebConfiguration implements WebMvcConfigurer {
 		return new BaseHandlerInterceptor();
 	}
 	
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		SimpleModule simpleModule = new SimpleModule();
+		simpleModule.addSerializer(BaseCodeLabelEnum.class, new BaseCodeLabelEnumJsonSerializer());
+		objectMapper.registerModule(simpleModule);
+		return objectMapper;
+	}
+	
+  @Bean
+  public MappingJackson2JsonView mappingJackson2JsonView() {
+    MappingJackson2JsonView jsonView = new MappingJackson2JsonView();
+    jsonView.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    jsonView.setObjectMapper(objectMapper());
+    return jsonView;
+  }
+  
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(baseHandlerInterceptor());
+	}
+	
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		// 페이지 리졸버 등록
+		resolvers.add(new MySQLPageRequestHandleMethodArgumentResolver());
 	}
 	
 }
